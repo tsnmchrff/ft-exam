d017ea8cf0c90bb8b5cb801aa5cf17cb05fb5d60
tsnmchrff <tasnim.chariffou@gmail.com>
2025-06-23 16:28:13 +0200

    feat(api): mise à jour du serveur avec Express

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const privateKey = "important_super_private_secret_key";
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ message: 'hello world' });
 });
 
+app.get('/random', (req, res) => {
+    const randomInt = Math.floor(Math.random() * 1000000);
+    res.json({ random: randomInt });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-}); 
+});
+
+// Remove me
