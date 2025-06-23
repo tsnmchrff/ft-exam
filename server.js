61f348d1ce2dd513cf45263e408bd6067ba2a71d
nooderg <theo.fenique@gmail.com>
2025-06-23 13:48:28 +0200

    adding secret key

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const privateKey = "important_super_private_secret_key";
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
